3e5aba1b20a4b9ca52284cd38096c19b623bd9aa
Vibhavee Trairattanapa <v.trairattanapa@criteo.com>
2017-03-30 13:31:30 +0800

    save & get template

--- index.js
+++ index.js
@@ -23,12 +23,85 @@ app.post('/api/save', function (req, res) {
 	console.log(req.body);
 
 	fs.writeFile("template.json", JSON.stringify(req.body), function(err) {
-	    if(err) {
-	    	console.log(err);
+		if(err) {
+			console.log(err);
 			res.json({'success': false});
-	    }
+		}
 
 		res.json({'success': true});
+	});
+
+})
+
+app.get('/api/get', function (req, res) {
+
+	fs.readFile(req.query.template + '.json', function read(err, data) {
+		if (err) {
+			throw err;
+		}
+		
+		res.json(JSON.parse(data));
+	});
+
+})
+
+function getData(path, data) {
+	if (!path) {
+		return null;
+	} 
+
+	var pathArray = path.split('\\');
+	console.log(pathArray)
+}
+
+function convert(mapping, data) {
+	var result = {
+		"account":{
+			"an": getData(mapping.account_app, data),
+			"cn": "us",
+			"ln": "en"
+		},
+		"site_type": "aa",
+		"id": {
+			"gaid": "e16332c1-dd78-4288-a4e3-6190ed632b7e"
+		},
+		"events": [
+			{
+				"event": "trackTransaction",
+				"dd": 1,
+				"nc ": 0,
+				"id": "transaction-uid",
+				"currency": "USD",
+				"product": [
+					{
+						"id": "1234",
+						"price": 10.2,
+						"quantity": 1
+					},
+					{
+						"id": "2345",
+						"price": 11.2,
+						"quantity": 2
+					}
+				]
+			}
+		],
+		"ip":"192.168.0.1",
+		"version":"s2s_v1.0.0"
+	};
+
+}
+
+app.get('/api/convert', function (req, res) {
+	// should we handle file not found?
+	fs.readFile(req.query.template + '.json', function read(err, data) {
+		if (err) {
+			throw err;
+		}
+		
+		var json = JSON.parse(data);
+
+		convert(json.mapping, req.query.data);
 	});
 
 })
--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,7 @@ $('#edit-template-modal').on('click', '.add-button button', function() {
 	var depth = $(this).data('depth');
 	var target = $(this).data('target');
 	$(target).append(`
-		<div class="field-box" style="margin-left: ' +depth+ 'px">
+		<div class="field-box extra-field-box" style="margin-left: ' +depth+ 'px">
 			<input type="text" class="form-control extra-input" placeholder="extra_field1">
 			<div class="drop-box" id="extra1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 			<button type="button" class="btn btn-link remove-field-button"><i class="glyphicon glyphicon-remove"></i></button>
@@ -34,7 +34,7 @@ $('#edit-template-modal').on('click', '.add-button button', function() {
 
 function flattenSchema(name, schema, level, parentType, path) {
 	var result = [];
-	var offset = 20;
+	var offset = 30;
 
 	if (schema.type == 'array') {
 
@@ -95,6 +95,10 @@ function flattenSchema(name, schema, level, parentType, path) {
 
 function renderTree(boxes) {
 	$('.schema-tree').empty();
+	// console.log(boxes);
+	if (!boxes || boxes.length == 0) {
+		$('.schema-tree').html('<p class="text-center">Please insert sample data</p>');
+	}
 	for(i in boxes) {
 		if (i == 0) {
 			continue;
@@ -102,9 +106,9 @@ function renderTree(boxes) {
 		$('.schema-tree').append(`
 			<div class="field-box" id="field-box-`+boxes[i].level+`" style="margin-left: `+boxes[i].level+`px" ondrop="drop(event)" ondragover="allowDrop(event)">
 				<div class="drop-box-inner">
-					<button class="element" id="el`+i+`" draggable="true" ondragstart="drag(event)">`+boxes[i].path+` (`+boxes[i].type+`)</button>
+					<button class="element" id="el`+i+`" draggable="true" ondragstart="drag(event)" data-path="`+boxes[i].path+`">`+boxes[i].name+` (`+boxes[i].type+`)</button>
 				</div>
-				<span class="shadow">`+boxes[i].path+` (`+boxes[i].type+`)</span>
+				<span class="shadow">`+boxes[i].name+` (`+boxes[i].type+`)</span>
 			</div><!-- field-box -->`);
 	}
 }
@@ -113,9 +117,40 @@ function getTree(data) {
 	var input = $('#input').val();
 	$.get('/api/getSchema?json=' + input, function( data ) {
 		var result = flattenSchema('root', data.schema, 0, null, '');
-		console.log(data);
+		// console.log(data);
 		renderTree(result);
 	});
+}
+
+function getPath(element) {
+	var button = element.find('button');
+	return button.data('path');
+}
+
+function getMapping() {
+	var fields = [
+		'account_app',
+		'account_currency',
+		'account_language',
+		'site_type',
+		'gaid',
+		'event_type',
+		'currency',
+		'products',
+		'transaction_id',
+		'product_id',
+		'quantity',
+		'price'
+	];
+
+	var results = {};
+
+	for(i in fields) {
+		var path = getPath($('#' + fields[i]));
+		results[fields[i]] = path;
+	}
+
+	return results;
 }
 
 function save() {
@@ -126,10 +161,12 @@ function save() {
 		method: 'post',
 		contentType: 'application/json',
 		data: JSON.stringify({
-			'test': 1234
+			templateName: $('#template').val(),
+			mapping: mapping
 		}),
 		//TODO handle if failed
 		success: function(res) {
+			$('#edit-template-modal').modal('hide');
 			console.log(res);
 		},
 		dataType: 'json'
@@ -137,7 +174,19 @@ function save() {
 }
 
 $('#edit-template-modal').on('shown.bs.modal', function () {
-  getTree();
+ 	getTree();
 })
 
-save();
+$('.save').click(function() {
+	save();
+})
+
+$('.fire').click(function() {
+	var template = $('#template').val();
+	var data = $('#input').val();
+	$.get('/api/convert?template=' + template + '&data=' + data, function( data ) {
+		var result = flattenSchema('root', data.schema, 0, null, '');
+		// console.log(data);
+		$('#output').val(result);
+	});
+});

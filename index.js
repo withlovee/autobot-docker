5daedbd6455404c46dbf6f248a8467fc4b264e9c
Vibhavee Trairattanapa <v.trairattanapa@criteo.com>
2017-03-31 10:55:08 +0800

    title animation / add restart api

--- index.js
+++ index.js
@@ -172,6 +172,16 @@ app.get('/api/convert', function (req, res) {
 
 })
 
+app.get('/restart', function(req, res) {
+	if (req.query.pwd && req.query.pwd == 'trairat') {
+		process.nextTick(function () {
+			throw new Error;
+		});
+	} else {
+		res.json({ success: true })
+	}
+})
+
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!')
 })
